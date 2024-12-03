5fe04840c2e180f023c60b0bd0da7b066c52b54c
Albert Alvin <121321650+albertalvin8080@users.noreply.github.com>
2024-12-03 20:19:05 -0300

    wip: speech and location

---
+++ front-end/js/location.js
@@ -0,0 +1,12 @@
+"use strict";
+
+const map = L.map('map'); 
+// Initializes map
+
+map.setView([51.505, -0.09], 13); 
+// Sets initial coordinates and zoom level
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '© OpenStreetMap'
+}).addTo(map); 
