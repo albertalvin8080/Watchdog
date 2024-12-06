486a4176082d15f278857511bfb4a1a762f45634
joaoTorpe <victotorpe10@gmail.com>
2024-12-06 18:21:49 -0300

    Feat(Admin login): Admin service layer login method

--- back-end/src/main/java/org/featherlessbipeds/watchdog/service/AdminService.java
+++ back-end/src/main/java/org/featherlessbipeds/watchdog/service/AdminService.java
@@ -1,4 +1,19 @@
 package org.featherlessbipeds.watchdog.service;
 
+import lombok.RequiredArgsConstructor;
+import org.featherlessbipeds.watchdog.repository.AdminRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class AdminService {
+
+    private final AdminRepository repository;
+
+    public boolean login(String email, String password){
+
+        return repository.existsByEmailAndPasswordHash(email,password);
+
+    }
+
 }
