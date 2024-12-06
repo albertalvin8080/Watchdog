ccc7af62a1c3f6fa0f3aeadd1e164e2879888a92
joaoTorpe <victotorpe10@gmail.com>
2024-12-06 19:56:31 -0300

    Refactor: Adding new fields to Condominium entity (name,email,passwordHash)

--- back-end/src/main/java/org/featherlessbipeds/watchdog/entity/Condominium.java
+++ back-end/src/main/java/org/featherlessbipeds/watchdog/entity/Condominium.java
@@ -19,6 +19,12 @@ public class Condominium
     private Integer id;
     @Embedded
     private Location location;
+    @Column(name = "condominium_name", columnDefinition = "VARCHAR(100)", nullable = false)
+    private String name;
+    @Column(name = "condominium_email", columnDefinition = "VARCHAR(255)", nullable = false, unique = true)
+    private String email;
+    @Column(name = "condominium_password_hash", columnDefinition = "VARCHAR(255)", nullable = false)
+    private String passwordHash;
     @OneToMany(mappedBy = "condominium", fetch = FetchType.EAGER, cascade = {})
     private Set<Entrance> entranceSet;
     @OneToMany(mappedBy = "condominium", fetch = FetchType.EAGER, cascade = {})
