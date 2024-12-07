5933bd985f4a6e34bf8711c03fa56d092c181bdd
joaoTorpe <victotorpe10@gmail.com>
2024-12-06 22:04:25 -0300

    Feat(Register condominium): Condominium service layer

--- back-end/src/main/java/org/featherlessbipeds/watchdog/service/CondominiumService.java
+++ back-end/src/main/java/org/featherlessbipeds/watchdog/service/CondominiumService.java
@@ -1,4 +1,19 @@
 package org.featherlessbipeds.watchdog.service;
 
+import lombok.RequiredArgsConstructor;
+import org.featherlessbipeds.watchdog.entity.Condominium;
+import org.featherlessbipeds.watchdog.repository.CondominiumRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class CondominiumService {
+
+    private final CondominiumRepository repository;
+
+    public void registerCondominium(Condominium condominium){
+        repository.save(condominium);
+
+    }
+
 }
