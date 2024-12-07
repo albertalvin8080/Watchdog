9425ee060029dd062cf068df68b638d6401896ab
albertalvin8080 <albertalvin99@gmail.com>
2024-12-07 15:52:04 -0300

    wip: location register for condominium

--- front-end/js/form.js
+++ front-end/js/form.js
@@ -1,17 +1,19 @@
 "use strict";
 
+mapApp.hideMap();
+
 function hideAllForms() {
-    const forms = document.querySelectorAll('form');
+    const forms = document.querySelectorAll("form");
     forms.forEach(form => {
-        form.style.display = 'none';
+        form.style.display = "none";
     });
 }
 
 hideAllForms();
 
 function showForm(formId) {
-    const forms = document.querySelectorAll('form');
-    forms.forEach(form => form.style.display = 'none');
+    const forms = document.querySelectorAll("form");
+    forms.forEach(form => form.style.display = "none");
     const formToShow = document.getElementById(formId);
-    formToShow.style.display = 'block';
+    formToShow.style.display = "block";
 }
