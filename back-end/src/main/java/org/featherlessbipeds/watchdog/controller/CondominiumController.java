189c0839fde1ae2e241231d6057b816b14ca9dfd
joaoTorpe <victotorpe10@gmail.com>
2024-12-06 22:03:20 -0300

    Feat(Register condominium): Condominium controller

--- back-end/src/main/java/org/featherlessbipeds/watchdog/controller/CondominiumController.java
+++ back-end/src/main/java/org/featherlessbipeds/watchdog/controller/CondominiumController.java
@@ -1,4 +1,48 @@
 package org.featherlessbipeds.watchdog.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.featherlessbipeds.watchdog.DTO.CondominiumRegisterDTO;
+import org.featherlessbipeds.watchdog.entity.Condominium;
+import org.featherlessbipeds.watchdog.service.CondominiumService;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("condom")
+@RequiredArgsConstructor
 public class CondominiumController {
+
+    private final CondominiumService service;
+
+    @PostMapping("register")
+    public ResponseEntity<String> registerComdominium(@RequestBody CondominiumRegisterDTO condom){
+
+        Condominium condominium = new Condominium();
+        condominium.setLocation(condom.location());
+        condominium.setName(condom.name());
+        condominium.setEmail(condom.email());
+        //A senha ta sem hash, dps v isso
+        condominium.setPasswordHash(condom.passwordHash());
+
+        try{
+            service.registerCondominium(condominium);
+            return ResponseEntity.status(HttpStatus.CREATED).body("Condominio criado");
+        }
+        catch (DataIntegrityViolationException e){
+            e.printStackTrace();
+            return ResponseEntity.status(HttpStatus.CONFLICT).body("Erro de integridade (Talvez a entidade ja exista): "+e.getMessage());
+        }
+        catch ( Exception e){
+            e.printStackTrace();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("BOoooM a bomba explodiu: "+e.getMessage());
+        }
+    }
+
+
+
 }
