b69851d01310d15040042f8b6839e9bc242f9790
joaoTorpe <victotorpe10@gmail.com>
2024-12-06 23:07:00 -0300

    Feat(Util): from object to jsonString convertor

---
+++ back-end/src/main/java/org/featherlessbipeds/watchdog/Util/JsonUtil.java
@@ -0,0 +1,17 @@
+package org.featherlessbipeds.watchdog.Util;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class JsonUtil {
+
+
+    public static String asJsonString(Object obj) {
+        try {
+            ObjectMapper objectMapper = new ObjectMapper();
+            return objectMapper.writeValueAsString(obj);
+        } catch (Exception e) {
+            throw new RuntimeException("Erro ao converter objeto para JSON", e);
+        }
+    }
+
+}
