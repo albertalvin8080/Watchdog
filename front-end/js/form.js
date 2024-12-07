ae69655960ffdbaa4e3e6c64d46431aa6cd26c45
JoaoVitorGl <vitorgouveia06@gmail.com>
2024-12-07 14:22:01 -0300

    Add forms for login and register

---
+++ front-end/js/form.js
@@ -0,0 +1,15 @@
+function hideAllForms() {
+    const forms = document.querySelectorAll('form');
+    forms.forEach(form => {
+        form.style.display = 'none';
+    });
+}
+
+hideAllForms();
+
+function showForm(formId) {
+    const forms = document.querySelectorAll('form');
+    forms.forEach(form => form.style.display = 'none');
+    const formToShow = document.getElementById(formId);
+    formToShow.style.display = 'block';
+}
