be99d9f0585b53abc4e4243ab6483dcd2323236e
joaoTorpe <victotorpe10@gmail.com>
2024-12-25 16:09:36 -0300

    feat(Entrance login): service layer login method

--- back-end/src/main/java/org/featherlessbipeds/watchdog/service/EntranceService.java
+++ back-end/src/main/java/org/featherlessbipeds/watchdog/service/EntranceService.java
@@ -1,4 +1,18 @@
 package org.featherlessbipeds.watchdog.service;
 
+import lombok.RequiredArgsConstructor;
+import org.featherlessbipeds.watchdog.entity.Entrance;
+import org.featherlessbipeds.watchdog.repository.EntranceRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class EntranceService {
+
+    private final EntranceRepository repository;
+
+    public Entrance login(String email,String password){
+        return repository.findByEmailAndPasswordHash(email,password);
+    }
+
 }
