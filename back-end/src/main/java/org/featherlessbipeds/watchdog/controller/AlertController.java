eb365b38c4109e5b3996b9ab61d7bc3c9dc3b2e8
joaoTorpe <victotorpe10@gmail.com>
2025-01-02 18:44:19 -0300

    feat(Audio persitence): endpoint

--- back-end/src/main/java/org/featherlessbipeds/watchdog/controller/AlertController.java
+++ back-end/src/main/java/org/featherlessbipeds/watchdog/controller/AlertController.java
@@ -4,13 +4,16 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.featherlessbipeds.watchdog.dto.AlertRegisterDTO;
 import org.featherlessbipeds.watchdog.entity.Alert;
+import org.featherlessbipeds.watchdog.entity.DangerLevel;
 import org.featherlessbipeds.watchdog.service.AlertService;
 import org.featherlessbipeds.watchdog.util.JsonUtil;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.List;
 
 @RestController
@@ -29,8 +32,13 @@ public class AlertController
         return ResponseEntity.ok(service.findAll());
     }
 
-    @PostMapping(path = "/register")
-    public ResponseEntity<?> createAlert(@RequestBody AlertRegisterDTO alert){
+    @PostMapping(path = "/register", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<?> createAlert(
+            @RequestParam("dangerLevel") DangerLevel dangerLevel,
+            @RequestParam("entranceId") Integer entranceId,
+            @RequestParam("description") MultipartFile description) throws IOException {
+
+        AlertRegisterDTO alert = new AlertRegisterDTO(dangerLevel,description.getBytes(),entranceId);
 
         try {
            return ResponseEntity.status(HttpStatus.CREATED).body(service.createAlert(alert));
