42aa0b979f63cd81a88975ee000c6380a2d2830b
joaoTorpe <victotorpe10@gmail.com>
2025-03-12 23:12:30 -0300

    refactor: parentID

--- back-end/src/main/java/org/featherlessbipeds/watchdog/dto/AlertReinforceDto.java
+++ back-end/src/main/java/org/featherlessbipeds/watchdog/dto/AlertReinforceDto.java
@@ -2,6 +2,6 @@ package org.featherlessbipeds.watchdog.dto;
 
 import org.featherlessbipeds.watchdog.entity.DangerLevel;
 
-public record AlertReinforceDto(Integer previousEntranceId, Integer entranceId, DangerLevel dangerLevel, byte[] description, String transcript)
+public record AlertReinforceDto(Integer previousEntranceId, Integer entranceId, DangerLevel dangerLevel, byte[] description, String transcript,Integer alertId)
 {
 }
